b818787fe9401ef5e05bf93ac1e98ca7da5b040c
lubayb1 <bryanlubay1@gmail.com>
2020-11-14 17:16:48 -0800

    14 Nov Mac

--- hello/views.py
+++ hello/views.py
@@ -35,9 +35,9 @@ from bs4 import BeautifulSoup
 def get_data(request, state = "nv"):
 
     client = requests.session()
-    client.get('https://coronavirusapi.com/users/sign_in')
+    client.get('https://coronavirusapi.com/users/sign_in').cookies
 
-    return JsonResponse(data={'r' : client.get('https://coronavirusapi.com/users/sign_in').content.decode()})
+    return JsonResponse(data={'r' : client.get('https://coronavirusapi.com/users/sign_in').content.decode()[0]})
 
     csrftoken = client.cookies['_policydock_session']
     # csrfwtf = client.cookies.extract_cookies()
