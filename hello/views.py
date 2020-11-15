c5700da31f1c0319134d3dc317bf7828a1c392e0
lubayb1 <bryanlubay1@gmail.com>
2020-11-14 17:23:48 -0800

    14 Nov Mac

--- hello/views.py
+++ hello/views.py
@@ -37,7 +37,9 @@ def get_data(request, state = "nv"):
     client = requests.session()
     client.get('https://coronavirusapi.com/users/sign_in').cookies
 
-    return JsonResponse(data={'r' : client.get('https://coronavirusapi.com/users/sign_in').content.decode(), 'shit' : 'hello'})
+    temp = client.get('https://coronavirusapi.com/users/sign_in').content.decode()
+
+    return JsonResponse(data={'r' : temp, 'shit' : temp.find('csrf-token')})
 
     csrftoken = client.cookies['_policydock_session']
     # csrfwtf = client.cookies.extract_cookies()
